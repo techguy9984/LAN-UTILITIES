7dfcdc648cfb462bd093d6b9a03da88d5689598d
WillDavies <wdavies973@gmail.com>
2016-12-08 20:56:21 -0600

    Updated command line and added file saving.

--- LAN-UTILITIES/src/com/cpjd/main/Main.java
+++ LAN-UTILITIES/src/com/cpjd/main/Main.java
@@ -1,62 +1,101 @@
 package com.cpjd.main;
 
 import java.util.ArrayList;
-import java.util.Random;
 import java.util.Scanner;
+
+import com.cpjd.main.Prefs.Save;
 
 public class Main {
 	
-	// Give them a score out of 1000, 1000 is the best, 0 hasn't played before 
-	private final String[] DATABASE = {
-			"Isaac:700","Joe:850","Will:800","Jake:750",
-			"John:200","Israel:100","Qui:650","Michael:200",
-			"IsaacM:600","Daniel:600","Kyle:100","Aaron:100"
-	};
+	private ArrayList<String> queue = new ArrayList<String>();
+	private ArrayList<String> database;
 	
-	private ArrayList<String> QUEUE = new ArrayList<String>();
-	
-	static boolean running;
+	private boolean running;
 	
 	public Main() {
 		running = true;
 
-		for(int i = 0; i < 10; i++) {
-			QUEUE.add(DATABASE[i]);
-		}
-		
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Welcome to the CS:GO Team finder. Type <help> for help.");
+		Prefs.initDirs();
+		System.out.println("Welcome to the lan utilities command line. Type <help> for help.");
 
 		while (running) {
 			String input = scanner.nextLine();
-
+			Save save = Prefs.deserializeSave();
+			if(save != null) database = save.getDatabase();
 			try {
 				if (input.equals("help")) {
-					System.out.println("Command: add <name>. Adds the player to the queue");
-					System.out.println("Command: find. Finds the teams");
-					System.out.println("Command: stop. Stops the program.");
-					System.out.println("Command: clear. Clears the queue.");
-					System.out.println("Command: push. Pushs the match to all the clients with the selected IP.");
-				} else if (input.startsWith("add")) {
+					System.out.println("Command						Description");
+					System.out.println("add <name>.					Adds the player to the queue");
+					System.out.println("find 						Finds the teams");
+					System.out.println("stop 						Stops the program.");
+					System.out.println("clear 						Clears the queue.");
+					System.out.println("push 						Pushs the match to all the clients with the selected IP.");
+					System.out.println("tol <#> 					Sets the tolerance level for match skill differences.");
+					System.out.println("setip 						Sets the IP / message to push to clients.");
+					System.out.println("dat 						Outputs all information in the database");
+					System.out.println("display 					Sets the number of viable matches to display. -1 for default.");
+					System.out.println("entry add <name> <skill> 			Adds a player to the database (overwrites existing). Skill should be between 0-1000.");
+					System.out.println("reset 						Resets all settings to the default.");
+				} else if(input.startsWith("add")) {
 					if(!addPlayer(input.split("\\s+")[1])) {
 						System.out.println("Player was not added to queue. Not found in database.");
 					}
-				} else if (input.startsWith("stop")) {
-					System.out.println("Stopping");
+				} else if(input.startsWith("stop")) {
+					System.out.println("Stopping app...");
 					running = false;
 					System.exit(0);
-				} else if (input.startsWith("find")) {
+				} else if(input.startsWith("find")) {
 					System.out.println("Finding teams...");
-					findV2(QUEUE);
+					MatchFinder.find(queue, save.getTolerance(), save.getDisplay());
+				} else if(input.startsWith("push")) {
+					
+				} else if(input.startsWith("tol")) {
+					save.setTolerance(Integer.parseInt(input.split("\\s+")[1]));
+					System.out.println("Tolerance set to "+save.getTolerance()+".");
+				} else if(input.startsWith("setip")) {
+					save.setMessage(input.split("\\s+")[1]);
+				} else if(input.startsWith("dat")) {
+					System.out.println("LOCAL DATABASE: ");
+					for(int i = 0; i < save.getDatabase().size(); i++) {
+						System.out.println(save.getDatabase().get(i));
+					}
+				} else if(input.startsWith("queue")) {
+					System.out.println("CURRENT QUEUE: ");
+					for(int i = 0; i < queue.size(); i++) {
+						System.out.println(queue.get(i));
+					}
+				} else if(input.startsWith("display")) {
+					save.setDisplay(Integer.parseInt(input.split("\\s+")[1]));
+					System.out.println("Number of viable matches to display set to "+save.getDisplay()+".");
+				} else if(input.startsWith("entry add")) {
+					boolean found = false;
+					if(save != null && save.getDatabase() != null) {
+						for (int i = 0; i < save.getDatabase().size(); i++) {
+							if(input.split("\\s+")[2].equalsIgnoreCase(save.getDatabase().get(i).split(":")[0])) {
+								System.out.println("Entry already exists. Overwriting.");
+								ArrayList<String> temp = save.getDatabase();
+								temp.set(i, input.split("\\s+")[2] + ":" + input.split("\\s+")[3]);
+								found = true;
+							}
+						}
+					}
+					if(!found) {
+						save.addDatabaseEntry(input.split("\\s+")[2]+":"+input.split("\\s+")[3]);
+						System.out.println("Added entry: "+input.split("\\s+")[2]+":"+input.split("\\s+")[3]+" to the database.");
+					}
+				} else if(input.startsWith("reset")) {
+					Prefs.deleteSave();
+					System.out.println("Preferences reset.");
 				}
 				else if(input.startsWith("clear")) {
-					QUEUE.clear();
+					queue.clear();
 					System.out.println("Queue cleared.");
 				}
 			} catch (Exception e) {
-				e.printStackTrace();
-				System.out.println("Incorrect syntax");
+				System.out.println("Unrecognized command or incorrect syntax");
 			}
+			Prefs.serializeSave(save);
 			
 		}
 		scanner.close();
@@ -66,167 +105,19 @@ public class Main {
 		new Main();
 	}
 
+	/**
+	 * Adds a player to the queue.
+	 * @param name The player's name as specified in the database file.
+	 * @return True if player was added successfully
+	 */
 	private boolean addPlayer(String name) {
-		for(int i = 0; i < DATABASE.length; i++) {
-			if(DATABASE[i].split(":")[0].equals(name)) {
-				QUEUE.add(DATABASE[i]);
-				System.out.println("Player "+name+" added to the queue. "+QUEUE.size()+" Player(s) in queue.");
+		for(int i = 0; i < database.size(); i++) {
+			if(database.get(i).split(":")[0].equalsIgnoreCase(name)) {
+				queue.add(database.get(i));
+				System.out.println("Player "+name+" added to the queue. "+queue.size()+" Player(s) in queue.");
 				return true;
 			}
 		}
 		return false;
 	}
-
-	/**
-	 * Finds all the possible teams by using binary counting.
-	 * @param queue
-	 */
-	private void findV2(ArrayList<String> queue) {
-		ArrayList<String> chart = new ArrayList<String>();
-		
-		for(int i = 0; i < Math.pow(2, queue.size()); i++) {
-			String s = Integer.toBinaryString(i);
-			while(s.length() < queue.size()) {
-				s+="0";
-			}
-			chart.add(s);
-		}
-		
-		ArrayList<Match> matches = new ArrayList<Match>();
-		
-		// Process the possibilites
-		for(int row = 0; row < chart.size(); row++) {
-			Match match = new Match();
-			for(int col = 0; col < chart.get(row).length(); col++) {
-				if(chart.get(row).charAt(col) == '0') {
-					match.t1_players.add(queue.get(col).split(":")[0]);
-					match.t1_totalSkill += Integer.parseInt(queue.get(col).split(":")[1]);
-				} else {
-					match.t2_players.add(queue.get(col).split(":")[0]);
-					match.t2_totalSkill += Integer.parseInt(queue.get(col).split(":")[1]);
-				}
-			}
-			matches.add(match);
-		}
-
-		int tolerance = 50; // the max acceptable score tolerance
-		ArrayList<Match> viable = new ArrayList<Match>();
-		// Sort the matches
-		for(int i = 0; i < matches.size(); i++) {
-			if(Math.abs(matches.get(i).t1_totalSkill - matches.get(i).t2_totalSkill) <= tolerance) {
-				viable.add(matches.get(i));
-			}
-		}
-		
-		// Randomly pick one of them
-		Random r = new Random();
-		Match selected;
-		try {
-			 selected = viable.get(r.nextInt(viable.size()));
-		} catch(Exception e) {
-			System.out.println("Match not found. Try increasing match tolerance.");
-			return;
-		}
-		// Output to the user
-		System.out.println("Found a match: ");
-		printMatchInfo(selected);
-	}
-	
-	private void printMatchInfo(Match match) {
-			System.out.println("Team 1 skill: "+match.t1_totalSkill);
-			String t1 = "";
-			for(int j = 0; j < match.t1_players.size(); j++) {
-				t1 += match.t1_players.get(j)+", ";
-			}
-			System.out.println(t1);
-			System.out.println("Team 2 skill: "+match.t2_totalSkill);
-			String t2 = "";
-			for(int j = 0; j < match.t2_players.size(); j++) {
-				t2 += match.t2_players.get(j)+", ";
-			}
-			System.out.println(t2);
-	}
-	/*
-	private void printMatchesInfo(ArrayList<Match> matches) {
-		for(int i = 0; i < matches.size(); i++) {
-			System.out.println("Match "+i);
-			System.out.println("Team 1 skill: "+matches.get(i).t1_totalSkill);
-			String t1 = "";
-			for(int j = 0; j < matches.get(i).t1_players.size(); j++) {
-				t1 += "Player "+(j+1)+": "+matches.get(i).t1_players.get(j)+" ";
-			}
-			System.out.println(t1);
-			System.out.println("Team 2 skill: "+matches.get(i).t2_totalSkill);
-			String t2 = "";
-			for(int j = 0; j < matches.get(i).t2_players.size(); j++) {
-				t2 += "Player "+(j+1)+": "+matches.get(i).t2_players.get(j)+" ";
-			}
-			System.out.println(t2);
-		}
-	}*/
-	
-	/*
-	 * For the find method here's how it works:
-	 * We want to balance the scores on each team so that they are as close as possible.
-	 * The score we're aiming for is half of the total points on each side.
-	 */
-	private void find() {
-		System.out.println("QUEUE SIZE: "+QUEUE.size());
-		int points = 0;
-		for(int i = 0; i < QUEUE.size(); i++) {
-			points += Integer.parseInt(QUEUE.get(i).split(":")[1]);
-		}
-		double perTeam = points / 2;
-		System.out.println("Total team points: "+points+", Points per team: "+perTeam);
-		
-		// Find all possible permutations
-		ArrayList<Match> teams = new ArrayList<Match>();
-		
-		for(int i = 0; i < QUEUE.size(); i++) {
-			Match team = new Match();
-			team.t1_players.add(QUEUE.get(i));
-			for(int j = 0; j < QUEUE.size(); j++) {
-				if(j == i) continue;
-				team.t1_players.add(QUEUE.get(j));
-				for(int k = 0; k < QUEUE.size(); k++) {
-					if(k == i || k == j) continue;
-					team.t1_players.add(QUEUE.get(k));
-					for(int l = 0; l < QUEUE.size(); l++) {
-						if(l == k || l == j || l == i) continue;
-						team.t1_players.add(QUEUE.get(l));
-						for(int m = 0; m < QUEUE.size(); m++) {
-							if(m == l || m == k || m == j || m == i) continue;
-							team.t1_players.add(QUEUE.get(m));
-						}
-					}
-				}
-			}
-			for(int j = 0; j < team.t1_players.size(); j++) {
-				for(int k = 0; k < QUEUE.size(); k++) {
-					if(QUEUE.get(k).split(":")[0].equals(team.t1_players.get(j))) continue;
-					team.t2_players.add(QUEUE.get(k).split(":")[0]);
-					team.t2_totalSkill += Integer.parseInt(QUEUE.get(k).split(":")[1]);
-				}
-			}
-			teams.add(team);
-			QUEUE.remove(0);
-		}
-		System.out.println("There are "+teams.size()+" possibilites");
-		print(teams);
-	}
-	
-	private void print(ArrayList<Match> teams) {
-		for(int i = 0; i < teams.size(); i++) {
-		}
-	}
-	
-	public class Match {
-		public ArrayList<String> t1_players = new ArrayList<String>();
-		public int t1_totalSkill = 0;
-		
-		public ArrayList<String> t2_players = new ArrayList<String>();
-		public int t2_totalSkill = 0;
-		
-	}
-
 }
---
+++ LAN-UTILITIES/src/com/cpjd/main/Prefs.java
@@ -0,0 +1,138 @@
+package com.cpjd.main;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ * Saves the program's preferences.
+ * @author Will Davies
+ *
+ */
+public class Prefs {
+
+private static File dir;
+	
+	/**
+	 * Should be called whenever the application is started.
+	 * This will locate the save dirs / create them in accordance with the os
+	 */
+	public static void initDirs() {
+		String osName = System.getProperty("os.name").toLowerCase();
+		
+		if(osName.contains("win")) {
+			dir = new File((System.getenv("APPDATA") + File.separator + "LAN-UTILS" + File.separator));
+		} else if(osName.contains("mac")) {
+			dir = new File(System.getProperty("user.home") + "/Library/Application Support/LAN-UTILS"+File.separator);
+		} else if(osName.contains("nux")) {
+			dir = new File(System.getProperty("user.home") + "/LAN-UTILS");
+		}
+		
+		if(!dir.exists()) dir.mkdir();
+	}
+	
+	public static boolean serializeSave(Save save) {
+		return serializeObject(save, "lan-save.ser");
+	}
+	
+	public static Save deserializeSave() {
+		Save save = (Save) deserializeObject("lan-save.ser");
+		if(save == null) {
+			save = new Save();
+			save.setDisplay(-1);
+			save.setTolerance(75);
+			serializeSave(save);
+		}
+		return save;
+	}
+	public static void deleteSave() {
+		deleteFile("lan-save.ser");
+	}
+	
+	private static boolean serializeObject(Object object, String fileName) {
+		try {
+			FileOutputStream fos = new FileOutputStream(dir + File.separator + fileName);
+			ObjectOutputStream out = new ObjectOutputStream(fos);
+			out.writeObject(object);
+			out.close();
+			fos.close();
+			return true;
+		} catch(Exception e) {
+			System.out.println("Could not find the droids you're looking for.");
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	private static Object deserializeObject(String location) {
+		try {
+			FileInputStream fis = new FileInputStream(dir + File.separator + location);
+			ObjectInputStream in = new ObjectInputStream(fis);
+			Object o = in.readObject();
+			in.close();
+			fis.close();
+			return o;
+		} catch(Exception e) {
+			System.out.println("Could not find the droids you're looking for.");
+			return null;
+		}
+	}
+	
+	private static void deleteFile(String location) {
+		File file = new File(dir + File.separator + location);
+		file.delete();
+	}
+	
+	public static class Save implements Serializable {
+		
+		private static final long serialVersionUID = 5042436675881979222L;
+		
+		public ArrayList<String> database;
+		public String message;
+		public int tolerance;
+		public int display;
+		
+		public int getDisplay() {
+			return display;
+		}
+		
+		public void setDisplay(int display) {
+			this.display = display;
+		}
+		
+		public void setDatabase(ArrayList<String> database) {
+			this.database = database;
+		}
+		
+		public ArrayList<String> getDatabase() {
+			return database;
+		}
+		
+		public void addDatabaseEntry(String entry) {
+			if(database == null) database = new ArrayList<String>();
+			database.add(entry);
+		}
+		
+		public void setTolerance(int tolerance) {
+			this.tolerance = tolerance;
+		}
+		
+		public int getTolerance() {
+			return tolerance;
+		}
+		
+		public void setMessage(String message) {
+			this.message = message;
+		}
+		
+		public String getMessage() {
+			return message;
+		}
+		
+	}
+}
+
